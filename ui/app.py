343bf2e2eeaa0608b9a61ac95d716c8a6ab9534f
Ze Wang <wangze19910407@gmail.com>
2026-05-30 09:14:47 +0800

    app.py add duration

--- ui/app.py
+++ ui/app.py
@@ -70,11 +70,12 @@ class VerilogQuizApp:
         self.current_question_index = question_index
         self.show_question_view(self.current_week, question_index)
     
-    def show_snackbar(self, message: str, color=ft.Colors.BLUE):
+    def show_snackbar(self, message: str, color=ft.Colors.BLUE, duration=None):
         """Show snackbar"""
         snack = ft.SnackBar(
             content=ft.Text(message),
             bgcolor=color,
+            duration=duration,
         )
         self.page.overlay.append(snack)
         snack.open = True
