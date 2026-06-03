cac7a82569a6c69e5e7386feed38e5285dbf904b
Ze Wang <wangze19910407@gmail.com>
2026-06-03 13:54:22 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,7 +10,7 @@ cp "$SCRIPT_DIR/$APP_NAME.desktop" ~/.local/share/applications/
 cp "$SCRIPT_DIR/$APP_NAME.png" ~/.local/share/icons/hicolor/512x512/apps/
 chmod +x ~/.local/share/applications/$APP_NAME.desktop
 
-sed -i "s|Exec=.*|Exec=$SCRIPT_DIR/$APP_NAME|" ~/.local/share/applications/$APP_NAME.desktop
+sed -i "s|Exec=.*|Exec=\"$SCRIPT_DIR/$APP_NAME\"|" ~/.local/share/applications/$APP_NAME.desktop
 
 update-desktop-database ~/.local/share/applications/ 2>/dev/null || true
 
